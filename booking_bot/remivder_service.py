885c717c44ad64c333f878654378e8a25a76b561
Anton <88505337+finchren@users.noreply.github.com>
2023-06-14 21:50:57 +0300

    Updated the reminder service to use the new DB format

--- booking_bot/remivder_service.py
+++ booking_bot/remivder_service.py
@@ -8,8 +8,7 @@ from telegram import Bot
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Initialize the bot
-bot = Bot(token='YOUR_API_KEY_GOES_HERE')
+bot = Bot(token='KEY')
 
 # Function to send start reminders
 def send_start_reminders():
@@ -22,12 +21,12 @@ def send_start_reminders():
     time_in_15_min = now + datetime.timedelta(minutes=15)
 
     # Retrieve bookings that are due to start in 15 minutes on the same day
-    c.execute("SELECT * FROM bookings WHERE booking_date = ? AND start_time = ?", (now.strftime('%Y-%m-%d'), time_in_15_min.strftime('%H:%M')))
+    c.execute("SELECT * FROM bookings WHERE start_booking_date = ? AND start_time = ?", (now.strftime('%d.%m.%Y'), time_in_15_min.strftime('%H:%M')))
     bookings = c.fetchall()
 
     # Send a reminder for each booking
     for booking in bookings:
-        id, user_id, booking_date, start_time, end_time = booking
+        id, user_id, _, _, start_time, end_time = booking
         bot.send_message(chat_id=user_id, text=f"Reminder: Your booking is due to start at {start_time}")
 
     # Close the SQLite connection
@@ -40,11 +39,11 @@ def send_end_reminders():
 
     now = datetime.datetime.now()
 
-    c.execute("SELECT * FROM bookings WHERE booking_date = ? AND end_time = ?", (now.strftime('%Y-%m-%d'), now.strftime('%H:%M')))
+    c.execute("SELECT * FROM bookings WHERE end_booking_date = ? AND end_time = ?", (now.strftime('%d.%m.%Y'), now.strftime('%H:%M')))
     bookings = c.fetchall()
     
     for booking in bookings:
-        id, user_id, booking_date, start_time, end_time = booking
+        id, user_id, _, _, start_time, end_time = booking
         bot.send_message(chat_id=user_id, text=f"Your booking has ended at {end_time}. Thank you for using our service.")
 
     conn.close()
